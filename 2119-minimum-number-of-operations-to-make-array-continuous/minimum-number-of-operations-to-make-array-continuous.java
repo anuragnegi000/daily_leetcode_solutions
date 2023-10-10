9763c31de0f392a290684ee3ff80a7eaefd22b06
Anurag Negi <115611556+anuragnegi000@users.noreply.github.com>
2023-10-11 02:08:50 +0530

    Time: 61 ms (37.86%) | Memory: 58.2 MB (32.04%) - LeetSync

---
+++ 2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+class Solution {
+    public int minOperations(int[] nums) {
+        Arrays.sort(nums);
+        int m = removeDuplicates(nums);
+        int n = nums.length;
+        int ans = n;
+
+        for (int i = 0; i < m; ++i) {
+            int j = upperBound(nums, i, m, nums[i] + n - 1);
+            ans = Math.min(ans, n - (j - i));
+        }
+
+        return ans;
+    }
+
+    // Helper method to remove duplicates and return the new size
+    private int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        int newSize = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                nums[newSize++] = nums[i];
+            }
+        }
+
+        return newSize;
+    }
+
+    // Helper method to find upper bound
+    private int upperBound(int[] nums, int left, int right, int target) {
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return right;
+    }
+}
